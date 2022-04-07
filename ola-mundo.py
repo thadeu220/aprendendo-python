b02036681e71441a3fae9e7f6ecf74321115129b
thadeu220 <ardennto@gmail.com>
2022-04-07 17:46:56 -0300

    adicionado mais coisas no meu arquivo ola-mundo.py, agora ele dá suporte a leitura e escrita de comandos e ainda oferece capacidades para converter um string para int

---
+++ ola-mundo.py
@@ -0,0 +1,18 @@
+# primeiro código feito na linguagem de programação python
+# toda a evolução precisa de um começo, eis o meu começo agora
+# autor: Thadeu Henrique dos anjos
+
+print("olá mundo, isso é um texto para o python")
+print("um passo pequeno para uma pessoa")
+print("mas um passo gigante para a humanidade")
+print("aposto que você entendeu que isso não é um olá mundo comum")
+print("então por favor, se apresente... qual o seu nome?")
+nome = input("insira o nome")
+print("é um prazer te conhecer ", nome,
+      " Sabia que as bebidas sem álcool tem 20x mais açúcar?")
+idadeInserida = input("qual a sua idade?")
+idade = int(idadeInserida)
+if(idade < 18):
+    print("sabia que para todo o caso de menoridade tem uma cadeia especial? Você deveria ser preso por beber sendo menor de idade")
+if(idade > 18):
+    print("eu sei que você gosta de tomar aquela cerveja no fim de semana, vamos beber juntos?")
